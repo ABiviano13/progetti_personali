84603fcc4ccab33fe79f7677f8edbc2e57586465
ABiviano13 <abiviano0@outlook.com>
2023-05-10 21:34:22 +0200

    FEAT : Header

--- personal-portfolio/src/components/portfolio/HeaderApp.vue
+++ personal-portfolio/src/components/portfolio/HeaderApp.vue
@@ -12,25 +12,52 @@ export default {
 
 <template>
 
-    <div class="container">
-        <div class="navbar">
-            <ul class="list_links">
-                <li class="item_link">
-                    Chi sono
-                </li>
-                <li class="item_link">
-                    I miei Progetti
-                </li>
-                <li class="item_link">
-                    Github
-                </li>
-            </ul>
+    <header class="main-header">
+
+        <div class="container">
+            <div class="navbar">
+                <h1 class="title_nav">
+                    Il mio portfolio
+                </h1>
+
+                <ul class="list_links">
+                    <li class="list_item_link">
+                        <a href="#">
+                            Chi sono
+                        </a>
+                    </li>
+                    <li class="list_item_link">
+                        <a href="#">
+                            I miei progetti
+                        </a>
+                    </li>
+                    <li class="list_item_link">
+                        <a href="#">
+                            Github
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
+
+    </header>
 
 </template>
 
 <style lang="scss" scoped>
 
+.main-header{
+    border-bottom: 1px solid;
+    color: rgb(77, 144, 188);;
+    .navbar{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
 
+        .list_links{
+            display: flex;
+            gap: 10px;
+        }
+    }
+}
 </style>
